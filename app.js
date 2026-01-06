827c71dfee5561c99c38af94dfe7dab5e7024068
whdbwhgks1248-gif <whdbwhgks1248@gmail.com>
2026-01-06 15:13:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const APPS_SCRIPT_URL = "PASTE_YOUR_WEB_APP_URL_HERE";
 const PEOPLE = ["영수","연실","한나","유나","아라","현아","건"];
 const CATEGORIES = ["택시","식당","기념품","카페","편의점","베이커리"];
 const DAYS = ["1","2","3","4","5"];
-const SETTLE_API_URL = "https://script.google.com/macros/s/AKfycbyh7hBFTDuuqRVEW06hFfyfk2h_RceeatYy4ko2h82tG2Rdpu5XAsme4Q5WZFbmNry7Kw/exec";
+const SETTLE_API_URL = "https://script.google.com/macros/s/AKfycbxE-ZtImmHbTVvReByoRiGgkrZLyeD5vOYxV74UAHLUxerhMqCf0qBGApeCDTLGNo3DHg/exec";
 
 // --- helpers ---
 const $ = (sel)=>document.querySelector(sel);
